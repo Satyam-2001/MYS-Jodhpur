2ace9ee8e5c7e7021c730b0c8931ef0abffc92e0
Satyam-2001 <lohiyasatyam@gmail.com>
2024-07-20 00:46:52 +0530

    service worker added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './services/http'
 import { Provider } from 'react-redux'
 import store from './store'
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import './index.css'
 import { ColorModeProvider } from './theme';
@@ -23,3 +23,5 @@ createRoot(document.getElementById("root")).render(
     </Provider>
   </QueryClientProvider>
 );
+
+serviceWorkerRegistration.unregister();
